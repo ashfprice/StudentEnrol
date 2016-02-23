bccbaf60a4bf246f93b9863a860761e2840ad770
ashfprice <b4035277@my.shu.ac.uk>
2016-02-23 16:28:08 +0000

    Tested the class Course, Imolemented scaffolding and bootstrapping

--- StudentEnrol/grails-app/init/BootStrap.groovy
+++ StudentEnrol/grails-app/init/BootStrap.groovy
@@ -1,7 +1,34 @@
+import com.studentenrol.*
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
+    
+def course1= new Course( title:'Interactive Media with Animation',
+                    department:'Computing',
+                    description:'"Lorem ipsum dolor sit amet, ad sea solum brute sensibus, te summo senserit vix, mel reque corpora vulputate ei."',
+                    leader:'Joe Jojo',
+                    code:'IMA101',
+                    startDate: new Date('19/01/2015'),
+                    endDate: new Date('19/01/2020')).save()
+
+def course2= new Course( title:'Professional Computing Project',
+                    department:'Computing',
+                    description:'"Lorem ipsum dolor sit amet, ad sea solum brute sensibus, te summo senserit vix, mel reque corpora vulputate ei."',
+                    leader:'Chloe Acland',
+                    code:'PCP101',
+                    startDate: new Date('19/01/2015'),
+                    endDate: new Date('19/01/2020')).save()
+
+def course3= new Course( title:'Web Architecture',
+                    department:'Computing',
+                    description:'"Lorem ipsum dolor sit amet, ad sea solum brute sensibus, te summo senserit vix, mel reque corpora vulputate ei."',
+                    leader:'Dom Woodward',
+                    code:'WAR101',
+                    startDate: new Date('19/01/2015'),
+                    endDate: new Date('19/01/2020')).save()
+
+}
     def destroy = {
     }
 }
