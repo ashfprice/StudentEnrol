c9d4747e65c5c1dbad4c28db0867770cbac00b55
ashfprice <b4035277@my.shu.ac.uk>
2016-03-08 12:49:49 +0000

    Tested the class Student, Implemented scaffolding and bootstrapping

--- StudentEnrol/grails-app/init/BootStrap.groovy
+++ StudentEnrol/grails-app/init/BootStrap.groovy
@@ -28,6 +28,30 @@ def course3= new Course( title:'Web Architecture',
                     startDate: new Date('19/01/2015'),
                     endDate: new Date('19/01/2020')).save()
 
+    
+def student1= new Student( name:'Ash Price',
+                    studentid:'B4035277',
+                    email:'b4035277@my.shu.ac.uk',
+                    username:'B4035277',
+                    password:'Password1',
+                    dob: new Date('16/07/1995'),
+                    course:'Computing').save()
+
+def student2= new Student(name:'Will Smith',
+                    studentid:'B4035266',
+                    email:'b4035266@my.shu.ac.uk',
+                    username:'B4035266',
+                    password:'Password2',
+                    dob: new Date('10/05/1996'),
+                    course:'Computing').save()
+
+def student3= new Student( name:'Jamie Wilkinson',
+                    studentid:'B4035255',
+                    email:'b4035255@my.shu.ac.uk',
+                    username:'B4035255',
+                    password:'Password3',
+                    dob: new Date('11/08/1992'),
+                    course:'Computing').save()
 }
     def destroy = {
     }
